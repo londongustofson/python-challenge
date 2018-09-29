791b84b2ea7e543b9bd52a0ff3a7c9af52ee0560
London Gustofson <londongustofson@gmail.com>
2018-09-29 15:01:09 -0700

    first commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,57 +1,65 @@
-import cvs
 
-csvfile = input("03-Python_homework_PyBank_Resources_budget_data (2).csv : ")
-# budget_data_1.csv
+import csv
+import os
 
-data = pd.read_csv("03-Python_homework_PyBank_Resources_budget_data (2).csv")
-countmonth = data["Date"].count()-1
-total_rev = data["Revenue"].sum()
-print("Financial Analysis")
-print("----------------------------")
-print("Total Months: " + str(countmonth) )
-print("Total Revenue: $" + str(total_rev) )
-#--------------------------get change, total, min, max------------------------
-revchange = []
-dictt = {}
-count = 0
+file_path= "03-Python_homework_PyBank_Resources_budget_data (2).csv"
 
+
+
+
+
+
+
+
+
+
+
+
+profit = []
+monthly_changes = []
 date = []
-Date = data["Date"]
-rev = data["Revenue"]
-for i in range(0 , len(rev)-1):
-    revchange.append(rev[i+1] - rev[i])
-# get number of change
-for j in revchange:
+count = 0
+total_profit = 0
+total_change_profits = 0
+initial_profit = 0
+with open(file_path,newline ="") as csvfile:
+  csvreader = csv.reader(csvfile, delimiter= ",")
+  csv_header = next(csvreader)
+  for row in csvreader:
     count = count + 1
-# get total change
-total_change = sum(revchange)
-# avg change
-avgRev_change = total_change / count
-print("Average Revenue Change: $" + str(avgRev_change))
-for a in range(0, len(revchange) -1):
-    date.append(Date[a])
+    date.append(row[0])
+    profit.append(row[1])
+    total_profit = total_profit + int(row[1])
+    final_profit = int(row[1])
+    monthly_change_profits = final_profit - initial_profit
+    monthly_changes.append(monthly_change_profits)
+    total_change_profits = total_change_profits + monthly_change_profits
+    initial_profit = final_profit
+    average_change_profits = (total_change_profits/count)
+    greatest_increase_profits = max(monthly_changes)
+    greatest_decrease_profits = min(monthly_changes)
 
-for b in range(0, len(date) -1):
-    dictt[date[b]] = revchange[b]
-    
-max_incr = max(dictt.values())
-min_incr = min(dictt.values())  ## get the max and min value 
-# get month according to the value
-MaxMin_date = []
-for key, value in dictt.items():
-    if value == max_incr:
-        MaxMin_date.append(key)
-    if value == min_incr:
-        MaxMin_date.append(key)
-print("Greatest Increase in Revenue: " + str(MaxMin_date[0]) + " ($" + str(max_incr) + ")")
-print("Greatest Decrease in Revenue: " + str(MaxMin_date[1]) + " ($" + str(min_incr) + ")")
+    increase_date = date[monthly_changes.index(greatest_increase_profits)]
+    decrease_date = date[monthly_changes.index(greatest_decrease_profits)]
 
-f = open("Output_analysis.txt" , "w")
-f.write("Financial Analysis"+ "\n")
-f.write("----------------------------"+ "\n")
-f.write("Total Months: " + str(countmonth) + "\n")
-f.write("Total Revenue: $" + str(total_rev)+ "\n")
-f.write("Average Revenue Change: $" + str(avgRev_change)+ "\n")
-f.write("Greatest Increase in Revenue: " + str(MaxMin_date[0]) + " ($" + str(max_incr) + ")"+ "\n")
-f.write("Greatest Decrease in Revenue: " + str(MaxMin_date[1]) + " ($" + str(min_incr) + ")"+"\n")
-f.close()
+  print("----------------------------------------------------------")
+  print("Financial Analysis")
+  print("----------------------------------------------------------")
+  print("Total Months: " + str(count))
+  print("Total Profits: " + "$" + str(total_profit))
+  print("Average Change: " + "$" + str(int(average_change_profits)))
+  print("Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase_profits) + ")")
+  print("Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease_profits)+ ")")
+  print("----------------------------------------------------------")
+
+with open("financial_analysis.txt", "w") as text:
+  text.write("----------------------------------------------------------\n")
+  text.write("  Financial Analysis"+ "\n")
+  text.write("----------------------------------------------------------\n\n")
+  text.write("    Total Months: " + str(count) + "\n")
+  text.write("    Total Profits: " + "$" + str(total_profit) +"\n")
+  text.write("    Average Change: " + "$" + str(int(average_change_profits)) + "\n")
+  text.write("    Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase_profits) + ")\n")
+  text.write("    Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease_profits) + ")\n")
+  text.write("----------------------------------------------------------\n")
+
