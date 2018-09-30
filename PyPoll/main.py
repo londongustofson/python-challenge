e8300acaca723fc47313a3e47223695ff8a70427
London Gustofson <londongustofson@gmail.com>
2018-09-29 21:43:00 -0700

    first commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,4 +3,36 @@ import os
 
 file_path= "03-Python_homework_PyPoll_Resources_election_data (1).csv"
 
+total_votes=0
+cand_dict = {}
+winner=""
+winner_votes=0
 
+with open(file_path, newline="") as csvfile:
+	csvreader = csv.reader(csvfile, delimiter=",")
+
+	next(csvreader, None)
+
+	for row in csvreader:
+		voter_id = row[0]
+		county = str(row[1])
+		candidate = str(row[2])
+		total_votes = total_votes + 1
+		if candidate in cand_dict:
+			cand_dict[candidate] = cand_dict[candidate] + 1
+		else:
+			cand_dict[candidate] = 1
+			
+
+print("Election Results")
+print("-------------------------")
+print(f'Total Votes: {total_votes}')
+for key, value in cand_dict.items():
+	if (value > winner_votes):
+		winner_votes = value
+		winner = key
+	percent = '{percent:.3%}'.format(percent=value/total_votes)
+	print(f'{key}: {percent} ({value})')
+print("-------------------------")
+print(f'Winner: {winner}')
+print("-------------------------")
